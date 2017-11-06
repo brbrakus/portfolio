c22659216a5c9439d5f2fae1b749498688160daf
Bruna Brakus <brunabrakus@protonmail.com>
2017-11-06 22:03:15 +0100

    refacture structure

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,13 +3,20 @@ import ProfilePicture from 'components/ProfilePicture';
 
 import s from './styles.scss';
 
-const About = () => (
-  <div className={s.about}>
-    <h1 className={s.title}>Hi, I'm Bruna!</h1>
-    <h3 className={s.right}>graphic & web designer</h3>
-    <ProfilePicture src="#" />
+const Title = () => (
+  <div>
+    <h1>Hi, I'm Bruna!</h1>
+    <h3 className={s.subtitle}>
+      graphic & web
+      <br />designer
+    </h3>
+  </div>
+);
+
+const Description = () => (
+  <div>
     <h2 className={s.left}>What I do</h2>
-    <p className={s.left}>
+    <p>
       Helping to make the web a pretty place. The coincidence is that pretty
       things sell well. :D
       <br />
@@ -17,6 +24,14 @@ const About = () => (
       <br />
       <br />To find out more see my CV.
     </p>
+  </div>
+);
+
+const About = () => (
+  <div className={s.about}>
+    <Title />
+    <ProfilePicture src="#" />
+    <Description />
   </div>
 );
 
