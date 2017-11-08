08d166af85c9453f0e9d9b8408dafa114ff3042c
Bruna Brakus <brunabrakus@protonmail.com>
2017-11-08 11:37:24 +0100

    mobile about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,24 +5,24 @@ import s from './styles.scss';
 
 const Title = () => (
   <div>
-    <h1>Hi, I{"'"}m Bruna!</h1>
-    <h3 className={s.subtitle}>
-      graphic & web
-      <br />designer
-    </h3>
+    <h1 className={s.title}>Hi, I{"'"}m Bruna!</h1>
+    <h3 className={s.subtitle}>graphic & web designer</h3>
   </div>
 );
 
 const Description = () => (
   <div>
-    <h2 className={s.left}>What I do</h2>
-    <p>
+    <h2 className={s.heading}>What I do</h2>
+    <p className={s.description}>
       Helping to make the web a pretty place. The coincidence is that pretty
       things sell well. :D
       <br />
       <br />Also going graphic design and trying to make your brand stick out.
       <br />
-      <br />To find out more see my CV.
+      <br />To find out more{' '}
+      <a href="/" className={s.link}>
+        see my CV.
+      </a>
     </p>
   </div>
 );
