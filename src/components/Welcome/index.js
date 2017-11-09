91a8a45c5926f4b6cec637006c467ed9d4a66736
Bruna Brakus <brunabrakus@protonmail.com>
2017-11-09 21:33:00 +0100

    pr fix

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -4,16 +4,10 @@ import ProfilePicture from 'components/ProfilePicture';
 import images from 'assets/images';
 import s from './styles.scss';
 
-const Title = () => (
-  <div>
-    <h1 className={s.title}>Hi, I{"'"}m Bruna!</h1>
-    <h3 className={s.subtitle}>graphic & web designer</h3>
-  </div>
-);
-
 const Welcome = () => (
   <div className={s.wrapper}>
-    <Title />
+    <h1 className={s.title}>Hi, I{"'"}m Bruna!</h1>
+    <h3 className={s.subtitle}>graphic & web designer</h3>
     <ProfilePicture image={images.profile} />
   </div>
 );
