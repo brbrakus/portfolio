71375bf553a0275d409b774112abef85cf524152
Bruna Brakus <brunabrakus@protonmail.com>
2017-11-11 19:12:35 +0100

    pr fix

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,22 +8,22 @@ import s from './styles.scss';
 
 const Icons = () => (
   <div className={s.icons}>
-    <a className={s.link} href="/">
+    <a className={s.link} href="https://twitter.com/BrunaBrakus">
       <div className={s.tooltip}>Twitter</div>
       <TwitterIcon className={s.icon} />
     </a>
 
-    <a className={s.link} href="/">
+    <a className={s.link} href="https://dribbble.com/BrunaBrakus">
       <div className={s.tooltip}>Dribbble</div>
       <DribbleIcon className={s.icon} />
     </a>
 
-    <a className={s.link} href="/">
+    <a className={s.link} href="https://github.com/brbrakus">
       <div className={s.tooltip}>Github</div>
       <GithubIcon className={s.icon} />
     </a>
 
-    <a className={s.link} href="/">
+    <a className={s.link} href="https://codepen.io/brunabrakus/">
       <div className={s.tooltip}>Codepen</div>
       <CodepenIcon className={s.icon} />
     </a>
