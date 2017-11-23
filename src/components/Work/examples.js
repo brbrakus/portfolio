39e96f9ffd75d0b759db14c0514da0d72cb0fabc
Bruna Brakus <brunabrakus@protonmail.com>
2017-11-23 22:56:02 +0100

    refactor work componenet

---
+++ src/components/Work/examples.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    name: 'ADOmiš',
+    description: 'Static web site for Astronomical Society of Omiš.'
+  },
+  {
+    name: 'Refužo',
+    description: 'Brend design for helthy food grocery stores.'
+  }
+];
--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -1,28 +1,23 @@
 import { h } from 'preact';
+import ProgressiveImage from 'components/ProgressiveImage';
+import examples from './examples';
 
 import s from './styles.scss';
 
-const Examples = () => (
-  <div>
-    <div className={s.example}>
-      <a className={s.link} href="/">
-        <h3>ADOmiš</h3>
-        <p>Static web site for Astronomical Society of Omiš.</p>
-      </a>
-    </div>
-    <div className={s.example}>
-      <a className={s.link} href="/">
-        <h3>Refužo</h3>
-        <p>Brend design for helthy food grocery stores.</p>
-      </a>
-    </div>
+const Example = ({ example: { name, description } }) => (
+  <div className={s.example}>
+    <a className={s.link} href="/">
+      <h3>{name}</h3>
+      <p>{description}</p>
+    </a>
   </div>
 );
 
 const Work = () => (
   <div className={s.work}>
     <h2 className={s.title}>Work I am fond of</h2>
-    <Examples />
+    {examples.map(example => <Example example={example} />)}
+    <ProgressiveImage />
   </div>
 );
 
