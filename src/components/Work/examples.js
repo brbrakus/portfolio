d80ac8cd754f3da2e8178ac5751abb2f2cf7adc6
Bruna Brakus <brunabrakus@protonmail.com>
2018-01-23 19:16:26 +0100

    add example images

--- src/components/Work/examples.js
+++ src/components/Work/examples.js
@@ -5,5 +5,15 @@ export default [
     name: 'refuzo',
     link: '/',
     image: images.refuzo
+  },
+  {
+    name: 'monthly-budget',
+    link: '/',
+    image: images.monthlybudget
+  },
+  {
+    name: 'adomis',
+    link: '/',
+    image: images.adomis
   }
 ];
