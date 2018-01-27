43f3ea7a8aeae1e9305d878d3d9339c7e7c3bf0b
Bruna Brakus <brunabrakus@protonmail.com>
2018-01-27 21:53:53 +0100

    work section style and layout, new accent color

--- src/components/Work/examples.js
+++ src/components/Work/examples.js
@@ -2,18 +2,27 @@ import images from 'assets/images';
 
 export default [
   {
-    name: 'refuzo',
-    link: '/',
-    image: images.refuzo
+    name: 'Monthly Budget',
+    link: 'https://monthly-budget.surge.sh/',
+    image: images.monthlybudget,
+    description:
+      'Design for progressive web app that aloves you managing your budget incomes and expanses. Design was created in Figma app.',
+    title: 'App Design'
   },
   {
-    name: 'monthly-budget',
-    link: '/',
-    image: images.monthlybudget
+    name: 'Refužo',
+    link: 'https://www.facebook.com/refuzo/',
+    image: images.refuzo,
+    description:
+      'Brand design for healthy food stores based in Split (Croatia). By creating complete visual identity created design meaning for these stores. Also, I am managing company Facebook page.',
+    title: 'Brand Design'
   },
   {
-    name: 'adomis',
-    link: '/',
-    image: images.adomis
+    name: 'Adomis',
+    link: 'https://adomis.surge.sh/',
+    image: images.adomis,
+    description:
+      'Web design and implementation for Astronomical sociaty of Omiš.',
+    title: 'Web Design & Implamantation'
   }
 ];
--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -4,22 +4,30 @@ import examples from './examples';
 
 import s from './styles.scss';
 
-const Example = ({ example: { link, image } }) => (
-  <div>
-    <a href={link}>
-      <ProgressiveImage
-        className={s.exampleImage}
-        src={image.src}
-        placeholder={image.placeholder}
-        alt={image.alt}
-      />
+const Example = ({ example: { title, link, image, name, description } }) => (
+  <div className={s.work}>
+    <div className={s.line} />
+    <h3 className={s.title}>{name}</h3>
+    <p className={s.description}>{description}</p>
+    <div className={s.wrapper}>
+      <a className={s.link} href={link}>
+        <ProgressiveImage
+          className={s.exampleImage}
+          src={image.src}
+          placeholder={image.placeholder}
+          alt={image.alt}
+        />
+      </a>
+      <h4 className={s.verticalTitle}>{title}</h4>
+    </div>
+    <a href={link} className={s.link}>
+      See it here
     </a>
   </div>
 );
 
 const Work = () => (
   <div className={s.work}>
-    <h2 className={s.title}>Work I am fond of</h2>
     {examples.map(example => <Example example={example} />)}
   </div>
 );
