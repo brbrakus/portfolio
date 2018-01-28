d00f24ccdc92fdc2e0283fcc9d8e04a66f3c9cfc
Bruna Brakus <brunabrakus@protonmail.com>
2018-01-28 19:20:47 +0100

    profile picture fix, new welcome text

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -6,20 +6,25 @@ import images from 'assets/images';
 import s from './styles.scss';
 
 const Welcome = () => (
-  <div className={s.wrapper}>
-    <h1 className={s.title}>Hi, I{"'"}m Bruna!</h1>
+  <div className={s.welcome}>
+    <div className={s.wrapper}>
+      <h1 className={s.title}>Hi, I{"'"}m Bruna!</h1>
 
-    <h3 className={s.subtitle}>graphic & web designer</h3>
+      <h3 className={s.subtitle}>graphic & web designer</h3>
+    </div>
 
-    <ProfilePicture image={images.profile} />
+    <ProfilePicture image={images.profile} className={s.profilePicture} />
 
     <p className={s.description}>
       I{"'"}m helping to make the web a pretty place.
-      <br />Also going graphic design and trying to make your brand stick out.
+      <br />Also doing graphic design and trying to make your brand stick out.
+      <br />
       <br />To find out more{' '}
       <Link href="/cv" className={s.link}>
         see my CV.
       </Link>
+      <br />
+      <br /> Contact me here.
     </p>
   </div>
 );
