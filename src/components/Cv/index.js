d9319380382b484d92f3f3e0371906ab29a64fa5
Bruna Brakus <brunabrakus@protonmail.com>
2018-01-30 19:15:51 +0100

    add skill component

--- src/components/Cv/index.js
+++ src/components/Cv/index.js
@@ -1,66 +1,38 @@
 import { h } from 'preact';
 import { Link } from 'preact-router/match';
-
 import CloseButton from 'assets/icons/CloseButton.js';
+import skills from './skills';
 
 import s from './styles.scss';
+
+const Card = ({ title, description }) => (
+  <div className={s.card}>
+    <h2 className={s.cardTitle}>{title}</h2>
+    <p
+      className={s.cardDescription}
+      dangerouslySetInnerHTML={{ __html: description }}
+      // eslint-disable-line react/no-danger
+    />
+  </div>
+);
 
 const Cv = () => (
   <div className={s.cv}>
     <Link href="/" className={s.closeButton}>
       <CloseButton />
     </Link>
-    <div className={s.idWrapper}>
-      <h2 className={s.idTitile}>Bruna Brakus</h2>
-      <p className={s.idDescription}>
+
+    <div className={s.wrapper}>
+      <h2 className={s.title}>Bruna Brakus</h2>
+      <p className={s.description}>
         Split, Croatia
         <br />brunabrakus@protonmail.com
       </p>
     </div>
-
-    <div className={s.about}>
-      <div className={s.card}>
-        <h2 className={s.title}>Work experience</h2>
-        <p className={s.description}>
-          <strong>Croativno: Graphic designer</strong>
-          <br />HAUT d.o.o., Split
-          <br />
-          <br />
-          <strong>Refužo: Brand designer</strong>
-          <br />Eco-stil d.o.o., Split
-          <br />
-          <br />
-          <strong>Mid: Graphic Assistent</strong>
-          <br />Mid d.o.o., Split
-        </p>
-      </div>
-      <div className={s.card}>
-        <h2 className={s.title}>Education</h2>
-        <p className={s.description}>
-          <strong>Media tehnician</strong>
-          <br />School for Design, Graphics and
-          <br />Sustainable Construction
-          <br />Split, Croatia
-          <br />
-          <br />
-          <strong>Multimedia, Design and Applications</strong>
-          <br />University North
-          <br />Varaždin, Croatia
-        </p>
-      </div>
-      <div className={s.card}>
-        <h2 className={s.title}>Working in</h2>
-        <p className={s.description}>
-          Adobe Creative Cloud:
-          <br />Illustrator, Photoshop,
-          <br />InDesign, Premiere
-          <br />
-          <br />Sketch, Figma
-          <br />
-          <br />Web tehnologies:
-          <br />HTML 5, CSS 3 (Scss)
-        </p>
-      </div>
+    <div className={s.cards}>
+      {skills.map(({ title, description }) => (
+        <Card title={title} description={description} />
+      ))}
     </div>
   </div>
 );
---
+++ src/components/Cv/skills.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    title: 'Work experience',
+    description:
+      '<strong>Croativno: Graphic designer</strong><br />HAUT d.o.o., Split<br /><br /><br /><strong>Refužo: Brand designer</strong><br />Eco-stil d.o.o., Split<br /><br /><strong>Mid: Graphic Assistent</strong><br />Mid d.o.o., Split'
+  },
+  {
+    title: 'Education',
+    description:
+      '<strong>Media tehnician</strong><br />School for Design, Graphics and<br />Sustainable Construction<br />Split, Croatia<br /><br /><strong>Multimedia, Design and Applications</strong><br />University North<br />Varaždin, Croatia'
+  },
+  {
+    title: 'Working in',
+    description:
+      'Adobe Creative Cloud:<br />InDesign, Premiere<br /><br />Illustrator, Photoshop,<br />Sketch, Figma<br /><br />Web tehnologies:<br />HTML 5, CSS 3 (Scss)'
+  }
+];
