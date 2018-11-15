eb875205e2b037c07ea6009abf9143f8f88c350d
Bruna Brakus <brunabrakus@protonmail.com>
2018-11-15 14:10:57 +0100

    add work images

--- assets/images/index.js
+++ assets/images/index.js
@@ -1,33 +1,73 @@
 /* eslint-disable */
 export default {
   profile: {
-    placeholder: require('./profile-placeholder.jpg'),
+    placeholder: require('./profile_placeholder.jpg'),
     src: require('./profile.jpg'),
-    alt: 'profile-image',
+    alt: 'profile-image'
   },
   momentum1: {
-    placeholder: require('./momentum1-placeholder.jpg'),
+    placeholder: require('./momentum1_placeholder.jpg'),
     src: require('./momentum1.jpg'),
-    alt: 'momentum-1',
+    alt: 'momentum'
   },
   momentum2: {
-    placeholder: require('./momentum2-placeholder.jpg'),
-    src: require('./momentum2-placeholder.jpg'),
-    alt: 'momentum-2',
+    placeholder: require('./momentum2_placeholder.jpg'),
+    src: require('./momentum2_placeholder.jpg'),
+    alt: 'momentum'
   },
   momentum3: {
-    placeholder: require('./momentum3-placeholder.jpg'),
+    placeholder: require('./momentum3_placeholder.jpg'),
     src: require('./momentum3.jpg'),
-    alt: 'momentum-3',
+    alt: 'momentum'
   },
   refuzo: {
-    placeholder: require('./refuzo.jpg'),
+    placeholder: require('./refuzo_placeholder.jpg'),
     src: require('./refuzo.jpg'),
-    alt: 'refuzo',
+    alt: 'Refuzo'
   },
   adomis: {
-    placeholder: require('./adomis.jpg'),
-    src: require('./adomis.jpg'),
-    alt: 'adomis-web',
+    placeholder: require('./adomis_placeholder.jpg'),
+    src: require('./adomis_placeholder.jpg'),
+    alt: 'Astronomic sociaty of Omis webpage'
   },
-}
+  monthly_budget: {
+    placeholder: require('./monthly_budget_placehoalder.jpg'),
+    src: require('./monthly_budget.jpg'),
+    alt: 'Monthly budget PWA'
+  },
+  avax_web: {
+    placeholder: require('./avax_web_placeholder.jpg'),
+    src: require('./avax_web.jpg'),
+    alt: 'AVAX Rent a car webpage'
+  },
+  cvita: {
+    placeholder: require('./hotel_cvita_placeholder.jpg'),
+    src: require('./hotel_cvita_placeholder.jpg'),
+    alt: 'Hotel Cvita webpage'
+  },
+  augusta: {
+    placeholder: require('./augusta_placeholder.jpg'),
+    src: require('./augusta.jpg'),
+    alt: 'Augusta accommodation webpage'
+  },
+  opnt: {
+    placeholder: require('./opnt_placeholder.jpg'),
+    src: require('./opnt_placeholder.jpg'),
+    alt: 'Old Paths New Trails logo'
+  },
+  momentum_logo: {
+    placeholder: require('./momentum_logo_placeholder.jpg'),
+    src: require('./momentum_logo.jpg'),
+    alt: 'Momentum rental logo'
+  },
+  rmc: {
+    placeholder: require('./rmc_placeholder.jpg'),
+    src: require('./rmc.jpg'),
+    alt: 'Royal Marine Croatia logo'
+  },
+  avax_logo: {
+    placeholder: require('./avax_logo_placeholder.jpg'),
+    src: require('./avax_logo_placeholder.jpg'),
+    alt: 'Royal Marine Croatia logo'
+  }
+};
