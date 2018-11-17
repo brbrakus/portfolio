411b76ce3afa5240508b4a3412b4086c516b9aa7
Bruna Brakus <brunabrakus@protonmail.com>
2018-11-18 00:57:02 +0100

    fixes and sw

--- src/app.js
+++ src/app.js
@@ -2,7 +2,10 @@ import { h, render } from 'preact';
 import Router from 'preact-router';
 import Home from 'components/Home';
 import Cv from 'components/Cv';
+import * as OfflinePluginRuntime from 'offline-plugin/runtime';
 import './globals.scss';
+
+OfflinePluginRuntime.install();
 
 const App = () => (
   <Router>
