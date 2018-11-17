1ef689888e0f69a987b3c6f062f5e5eb1dd9254a
Bruna Brakus <brunabrakus@protonmail.com>
2018-11-17 11:20:36 +0100

    data update

--- src/components/Work/examples.js
+++ src/components/Work/examples.js
@@ -5,14 +5,14 @@ import momentumVideoUrl from 'assets/images/momenutm2.mp4';
 
 export default [
   {
-    name: 'Mometum',
+    name: 'Momentum',
     images: [
       { src: images.momentum1, isVideo: false },
       { src: momentumVideoUrl, isVideo: true },
       { src: images.momentum3, isVideo: false }
     ],
     description:
-      'I’m product designer in Momentum team. Momentum is a modern reservation booking platform designed to bring happy customers to growing rental business.'
+      'At the moment I’m working as a product designer in the Momentum team. Momentum is a modern booking platform with the goal of growing rental business. I’ve designed product brand, as well as UI/UX in the product’s CMS and user facing application.'
   },
   {
     name: 'UX & UI Design',
@@ -24,7 +24,7 @@ export default [
       { src: images.augusta, isVideo: false }
     ],
     description:
-      'I designed web pages for  such as restaurant and accommodation Augusta Insula, Hotel Cvita, Astronomical Society of Omiš and Monthly Budget PWA.'
+      'I provided solution for the modern budgeting application "Monthly Budget". In the need for presentational web sites, I also worked for the clients such as restaurant and accommodation Augusta Insula, Hotel Cvita and Astronomical Society of Omiš.'
   },
   {
     name: 'Branding',
@@ -36,6 +36,6 @@ export default [
       { src: images.avax_logo, isVideo: false }
     ],
     description:
-      'I illustrated visual indentity and helped grow a brand for companies like Old Paths New Trails, Royal Marine Croatia, Refužo, Avax rent a car...'
+      'I illustrated visual indentity and helped grow a brand for companies like Old Paths New Trails, Royal Marine Croatia, Refužo, Avax rent a car.'
   }
 ];
