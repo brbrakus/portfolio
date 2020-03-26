b816f28cf34139e74b0d097b0ac1daa285bb9b81
Miro Dojkic <mdojkic@gmail.com>
2020-03-27 00:17:53 +0100

    Fix default module export 👽

--- assets/images/index.js
+++ assets/images/index.js
@@ -1,103 +1,103 @@
 /* eslint-disable */
 export default {
   profile: {
-    placeholder: require('./profile_placeholder.jpg'),
-    src: require('./profile.jpg'),
+    placeholder: require('./profile_placeholder.jpg').default,
+    src: require('./profile.jpg').default,
     alt: 'profile-image'
   },
   momentum1: {
-    placeholder: require('./momentum1_placeholder.jpg'),
-    src: require('./momentum1.jpg'),
+    placeholder: require('./momentum1_placeholder.jpg').default,
+    src: require('./momentum1.jpg').default,
     alt: 'momentum'
   },
   momentum2: {
-    placeholder: require('./momentum2_placeholder.jpg'),
-    src: require('./momentum2_placeholder.jpg'),
+    placeholder: require('./momentum2_placeholder.jpg').default,
+    src: require('./momentum2_placeholder.jpg').default,
     alt: 'momentum'
   },
   momentum3: {
-    placeholder: require('./momentum3_placeholder.jpg'),
-    src: require('./momentum3.jpg'),
+    placeholder: require('./momentum3_placeholder.jpg').default,
+    src: require('./momentum3.jpg').default,
     alt: 'momentum'
   },
   refuzo: {
-    placeholder: require('./refuzo_placeholder.jpg'),
-    src: require('./refuzo.jpg'),
+    placeholder: require('./refuzo_placeholder.jpg').default,
+    src: require('./refuzo.jpg').default,
     alt: 'Refuzo'
   },
   adomis: {
-    placeholder: require('./adomis_placeholder.jpg'),
-    src: require('./adomis_placeholder.jpg'),
+    placeholder: require('./adomis_placeholder.jpg').default,
+    src: require('./adomis_placeholder.jpg').default,
     alt: 'Astronomic sociaty of Omis webpage'
   },
   monthly_budget: {
-    placeholder: require('./monthly_budget_placehoalder.jpg'),
-    src: require('./monthly_budget.jpg'),
+    placeholder: require('./monthly_budget_placehoalder.jpg').default,
+    src: require('./monthly_budget.jpg').default,
     alt: 'Monthly budget PWA'
   },
   avax_web: {
-    placeholder: require('./avax_web_placeholder.jpg'),
-    src: require('./avax_web.jpg'),
+    placeholder: require('./avax_web_placeholder.jpg').default,
+    src: require('./avax_web.jpg').default,
     alt: 'AVAX Rent a car webpage'
   },
   cvita: {
-    placeholder: require('./hotel_cvita.jpg'),
-    src: require('./hotel_cvita.jpg'),
+    placeholder: require('./hotel_cvita.jpg').default,
+    src: require('./hotel_cvita.jpg').default,
     alt: 'Hotel Cvita webpage'
   },
   augusta: {
-    placeholder: require('./augusta_placeholder.jpg'),
-    src: require('./augusta.jpg'),
+    placeholder: require('./augusta_placeholder.jpg').default,
+    src: require('./augusta.jpg').default,
     alt: 'Augusta accommodation webpage'
   },
   opnt: {
-    placeholder: require('./opnt_placeholder.jpg'),
-    src: require('./opnt_placeholder.jpg'),
+    placeholder: require('./opnt_placeholder.jpg').default,
+    src: require('./opnt_placeholder.jpg').default,
     alt: 'Old Paths New Trails logo'
   },
   momentum_logo: {
-    placeholder: require('./momentum_logo_placeholder.jpg'),
-    src: require('./momentum_logo.jpg'),
+    placeholder: require('./momentum_logo_placeholder.jpg').default,
+    src: require('./momentum_logo.jpg').default,
     alt: 'Momentum rental logo'
   },
   rmc: {
-    placeholder: require('./rmc_placeholder.jpg'),
-    src: require('./rmc.jpg'),
+    placeholder: require('./rmc_placeholder.jpg').default,
+    src: require('./rmc.jpg').default,
     alt: 'Royal Marine Croatia logo'
   },
   avax_logo: {
-    placeholder: require('./avax_logo_placeholder.jpg'),
-    src: require('./avax_logo.jpg'),
+    placeholder: require('./avax_logo_placeholder.jpg').default,
+    src: require('./avax_logo.jpg').default,
     alt: 'Royal Marine Croatia logo'
   },
   gusar: {
-    placeholder: require('./gusar-placeholder.jpg'),
-    src: require('./gusar.jpg'),
+    placeholder: require('./gusar-placeholder.jpg').default,
+    src: require('./gusar.jpg').default,
     alt: 'Gusar logo'
   },
   enduro_perun: {
-    placeholder: require('./enduro-perun-placeholder.jpg'),
-    src: require('./enduro-perun.jpg'),
+    placeholder: require('./enduro-perun-placeholder.jpg').default,
+    src: require('./enduro-perun.jpg').default,
     alt: 'Enduro Perun'
   },
   reel_app: {
-    placeholder: require('./reel-app-placeholder.jpg'),
-    src: require('./reel-app.jpg'),
+    placeholder: require('./reel-app-placeholder.jpg').default,
+    src: require('./reel-app.jpg').default,
     alt: 'Reel app'
   },
   reel_back_office: {
-    placeholder: require('./reel-back-office-placeholder.jpg'),
-    src: require('./reel-back-office.jpg'),
+    placeholder: require('./reel-back-office-placeholder.jpg').default,
+    src: require('./reel-back-office.jpg').default,
     alt: 'Reel back office'
   },
   reel_logo: {
-    placeholder: require('./reel-logo-placeholder.jpg'),
-    src: require('./reel-logo.jpg'),
+    placeholder: require('./reel-logo-placeholder.jpg').default,
+    src: require('./reel-logo.jpg').default,
     alt: 'Reel logo'
   },
   reel_landing_page: {
-    placeholder: require('./reel-lp-placeholder.jpg'),
-    src: require('./reel-lp.jpg'),
+    placeholder: require('./reel-lp-placeholder.jpg').default,
+    src: require('./reel-lp.jpg').default,
     alt: 'Reel landing page'
   }
 };
