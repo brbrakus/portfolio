84cf008db94f0b47e213ef7042c3fa99266dbb3b
Miro Dojkic <mdojkic@gmail.com>
2020-03-27 15:09:44 +0100

    Serve images from cloudinary 🍱

--- assets/images/index.js
+++ assets/images/index.js
@@ -2,102 +2,87 @@
 export default {
   profile: {
     placeholder: require('./profile_placeholder.jpg').default,
-    src: require('./profile.jpg').default,
-    alt: 'profile-image'
+    src: 'https://res.cloudinary.com/miroslav-dojkic/image/upload/v1585317546/profile.jpg',
+    alt: 'Bruna Brakus'
   },
   momentum1: {
     placeholder: require('./momentum1_placeholder.jpg').default,
-    src: require('./momentum1.jpg').default,
-    alt: 'momentum'
-  },
-  momentum2: {
-    placeholder: require('./momentum2_placeholder.jpg').default,
-    src: require('./momentum2_placeholder.jpg').default,
-    alt: 'momentum'
+    src: 'https://res.cloudinary.com/miroslav-dojkic/image/upload/v1585317547/momentum1.jpg',
+    alt: 'Momentum Vehicle Rental System'
   },
   momentum3: {
     placeholder: require('./momentum3_placeholder.jpg').default,
-    src: require('./momentum3.jpg').default,
-    alt: 'momentum'
+    src: 'https://res.cloudinary.com/miroslav-dojkic/image/upload/v1585317544/momentum3.jpg',
+    alt: 'Momentum Vehicle Rental System'
   },
   refuzo: {
     placeholder: require('./refuzo_placeholder.jpg').default,
-    src: require('./refuzo.jpg').default,
-    alt: 'Refuzo'
-  },
-  adomis: {
-    placeholder: require('./adomis_placeholder.jpg').default,
-    src: require('./adomis_placeholder.jpg').default,
-    alt: 'Astronomic sociaty of Omis webpage'
+    src: 'https://res.cloudinary.com/miroslav-dojkic/image/upload/v1585317558/refuzo.jpg',
+    alt: 'Refuzo Healthy Food Store'
   },
   monthly_budget: {
     placeholder: require('./monthly_budget_placehoalder.jpg').default,
-    src: require('./monthly_budget.jpg').default,
+    src: 'https://res.cloudinary.com/miroslav-dojkic/image/upload/v1585317543/monthly_budget.jpg',
     alt: 'Monthly budget PWA'
   },
   avax_web: {
     placeholder: require('./avax_web_placeholder.jpg').default,
-    src: require('./avax_web.jpg').default,
+    src: 'https://res.cloudinary.com/miroslav-dojkic/image/upload/v1585317547/avax_web.jpg',
     alt: 'AVAX Rent a car webpage'
   },
   cvita: {
-    placeholder: require('./hotel_cvita.jpg').default,
-    src: require('./hotel_cvita.jpg').default,
+    placeholder: 'https://res.cloudinary.com/miroslav-dojkic/image/upload/v1585317548/hotel_cvita.jpg',
+    src: 'https://res.cloudinary.com/miroslav-dojkic/image/upload/v1585317548/hotel_cvita.jpg',
     alt: 'Hotel Cvita webpage'
   },
   augusta: {
     placeholder: require('./augusta_placeholder.jpg').default,
-    src: require('./augusta.jpg').default,
+    src: 'https://res.cloudinary.com/miroslav-dojkic/image/upload/v1585317538/augusta.jpg',
     alt: 'Augusta accommodation webpage'
-  },
-  opnt: {
-    placeholder: require('./opnt_placeholder.jpg').default,
-    src: require('./opnt_placeholder.jpg').default,
-    alt: 'Old Paths New Trails logo'
   },
   momentum_logo: {
     placeholder: require('./momentum_logo_placeholder.jpg').default,
-    src: require('./momentum_logo.jpg').default,
+    src: 'https://res.cloudinary.com/miroslav-dojkic/image/upload/v1585317541/momentum_logo.jpg',
     alt: 'Momentum rental logo'
   },
   rmc: {
     placeholder: require('./rmc_placeholder.jpg').default,
-    src: require('./rmc.jpg').default,
+    src: 'https://res.cloudinary.com/miroslav-dojkic/image/upload/v1585317545/rmc.jpg',
     alt: 'Royal Marine Croatia logo'
   },
   avax_logo: {
     placeholder: require('./avax_logo_placeholder.jpg').default,
-    src: require('./avax_logo.jpg').default,
+    src: 'https://res.cloudinary.com/miroslav-dojkic/image/upload/v1585317534/avax_logo.jpg',
     alt: 'Royal Marine Croatia logo'
   },
   gusar: {
     placeholder: require('./gusar-placeholder.jpg').default,
-    src: require('./gusar.jpg').default,
+    src: 'https://res.cloudinary.com/miroslav-dojkic/image/upload/v1585317541/gusar.jpg',
     alt: 'Gusar logo'
   },
   enduro_perun: {
     placeholder: require('./enduro-perun-placeholder.jpg').default,
-    src: require('./enduro-perun.jpg').default,
+    src: 'https://res.cloudinary.com/miroslav-dojkic/image/upload/v1585317531/enduro-perun.jpg',
     alt: 'Enduro Perun'
   },
   reel_app: {
     placeholder: require('./reel-app-placeholder.jpg').default,
-    src: require('./reel-app.jpg').default,
+    src: 'https://res.cloudinary.com/miroslav-dojkic/image/upload/v1585317530/reel-app.jpg',
     alt: 'Reel app'
   },
   reel_back_office: {
     placeholder: require('./reel-back-office-placeholder.jpg').default,
-    src: require('./reel-back-office.jpg').default,
+    src: 'https://res.cloudinary.com/miroslav-dojkic/image/upload/v1585317549/reel-back-office.jpg',
     alt: 'Reel back office'
   },
   reel_logo: {
     placeholder: require('./reel-logo-placeholder.jpg').default,
-    src: require('./reel-logo.jpg').default,
+    src: 'https://res.cloudinary.com/miroslav-dojkic/image/upload/v1585317538/reel-logo.jpg',
     alt: 'Reel logo'
   },
   reel_landing_page: {
     placeholder: require('./reel-lp-placeholder.jpg').default,
-    src: require('./reel-lp.jpg').default,
+    src: 'https://res.cloudinary.com/miroslav-dojkic/image/upload/v1585317536/reel-lp.jpg',
     alt: 'Reel landing page'
   }
 };
