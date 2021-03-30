be20b2005356f320b6ad33ab774ab8b87e11d8f7
Miro Dojkic <mdojkic@gmail.com>
2021-03-30 02:11:28 +0200

    Change descriptions for examples 🍱

--- src/components/Work/examples.js
+++ src/components/Work/examples.js
@@ -13,7 +13,8 @@ const rentistic = {
     { src: images.rentistic_responsive_reservations, isVideo: false }
   ],
   description: `
-  Live bookings and occupancy calendars, price lists and seasons, channel managers for rentals, units and tours is just a part of functionalities that Rentistic is growing. My role is come up with ideas that meet product requirements, provide a good UX and can compete on the market. I’m collaborating on daily basis with stakeholders, project and product managers and team lead to reach the product goals. Also, I’m presenting  business logic through design to the rest of the team as well as collaborating with developers on the design handoff.
+  With Rentistic, users can manage live bookings with the help of occupancy calendars, create price lists and connect their businesses through channel managers for rentals, units and tours.<br><br>
+  My role is to come up with concepts that meet product requirements, provide a good UX and can compete with the market. I’m collaborating daily with stakeholders, product managers and team leads to reach the product goals. I’m also responsible for presenting the business logic through the design to the delivery team and providing developers with the design handoff.
   `
 };
 
@@ -27,8 +28,9 @@ const sugestio = {
     { src: images.sugestio_mobile_app, isVideo: false },
     { src: images.sugestio_web_app, isVideo: false }
   ],
-  description: `Sugestio is free tourist guidebook application that provides all the local highlights that accommodation host want to provide.
-My biggest challenge was to define concept for new version functionalities with payment options.  In two months I come up with concepts and solutions that aim to reach the product goals, define specification, prototype and present the design to the stakeholders. I continued working as needed on the improvements for the mobile and web application, building a style guide and redesigning components.`
+  description: `Sugestio is a free tourist guidebook that enables accommodation host to reveal all the local highlights to the guest.
+  <br><br>My biggest challenge was to come up with the concepts for the new features that would enable the application to bring in the profit.
+  In two months I came up with solutions, defined specification, prototyped and presented the design to the stakeholders. I continued working as needed on the improvements for the mobile and web application, creating a style guide and redesigning components.`
 };
 
 const reel = {
@@ -44,10 +46,11 @@ const reel = {
     { src: images.reel_app, isVideo: false },
     { src: images.reel_logo, isVideo: false }
   ],
-  description: `Condition report system for vehicle rental businesses. The mobile app automates the reporting process and web app allows an agency to track reports and manage their fleet, users and other business entities. Reel application won the 2nd place at the Get in the Ring startup competition in Split, Croatia.
-    <br><br>
-    My role was to help with the idea and design a visual identity, UX and UI for web and mobile apps. I've also implemented a landing page.
-    <br>`
+  description: `Condition report system for vehicle rental businesses. The mobile app automates the reporting process and web app allows an agency to track reports and manage their fleet, users and other business entities.
+  <br><br>
+  Reel application won the 2nd place at the Get in the Ring startup competition in Split, Croatia.
+  I helped with the idea and designed a visual identity, UX and UI for web and mobile apps. I've also implemented the landing page.
+  <br>`
 };
 
 const branding = {
@@ -60,7 +63,7 @@ const branding = {
     { src: images.avax_logo, isVideo: false }
   ],
   description:
-    "I illustrated visual identity and helped grow a brand for companies like Avax rent a car, TKD Gusar, Refužo, Old Paths New Trails, Royal Marine Croatia... <br /><br />As a personal project, I create social media content for Coast Riders MTB team. I'm helping with the organization of Enduro Perun bike fest which includes digital marketing campaigns, designing graphic materials, creating content for social media, event website design and more.<br />"
+    "I illustrated visual identity and helped grow a brand for companies like Avax rent a car, TKD Gusar, Refužo, Old Paths New Trails and Royal Marine Croatia. <br /><br />As a personal project, I'm helping with the organization of the Enduro Perun bike fest which includes creating digital marketing campaigns, designing graphic materials, creating content for social media, event website design and more.<br />"
 };
 
 export default [rentistic, sugestio, reel, branding];
